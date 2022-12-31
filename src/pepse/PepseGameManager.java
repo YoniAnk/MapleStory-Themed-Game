ed89a81a74815aa4220f0fd6c0724838a31e2bcd
Nir Balouka <nirblo50@gmail.com>
2022-12-31 14:15:17 +0200

    added tree trunks

--- src/pepse/PepseGameManager.java
+++ src/pepse/PepseGameManager.java
@@ -7,12 +7,18 @@ import danogl.gui.ImageReader;
 import danogl.gui.SoundReader;
 import danogl.gui.UserInputListener;
 import danogl.gui.WindowController;
+import danogl.gui.rendering.Camera;
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
+import pepse.util.ColorSupplier;
+import pepse.world.Block;
 import pepse.world.Sky;
 import pepse.world.Terrain;
 import pepse.world.daynight.Night;
 import pepse.world.daynight.Sun;
 import pepse.world.daynight.SunHalo;
+import pepse.world.trees.Tree;
 
 import java.awt.*;
 
@@ -20,38 +26,39 @@ public class PepseGameManager extends GameManager {
 
     /************ Game Settings Constants ***************/
     public static final String WINDOWS_NAME = "Pepse Game";
-    private static final int BOARD_HEIGHT = 700;
+    private static final int BOARD_HEIGHT = 720;
     private static final int BOARD_WIDTH = 1005;
 
     /************** day/night properties ***************/
 
-    private static final float NIGHT_CYCLE_LEN = 10;
+    private static final float NIGHT_CYCLE_LEN = 48;
     private static final float SUNSET_CYCLE = NIGHT_CYCLE_LEN * 2;
     private static final Color HALO_COLOR = new Color(255, 255, 0, 20);
 
     /************** Terrain properties ***************/
     public static final int TERRAIN_SEED = 1000;
-
+    private Vector2 windowDimensions;
+    private Terrain terrain;
 
     /************ Class Functions ***********/
 
     /**
      * The constructor of Pepse Game Manager
      *
-     * @param windowTitle The name of the window
+     * @param windowTitle      The name of the window
      * @param windowDimensions the windows dimension
      */
     public PepseGameManager(String windowTitle, Vector2 windowDimensions) {
-        super(windowTitle,windowDimensions);
+        super(windowTitle, windowDimensions);
     }
 
 
     /**
      * Initializes the PepseGameManager
      *
-     * @param imageReader the image reader to use for loading images
-     * @param soundReader the sound reader to use for loading sounds
-     * @param inputListener the user input listener to use for handling user input
+     * @param imageReader      the image reader to use for loading images
+     * @param soundReader      the sound reader to use for loading sounds
+     * @param inputListener    the user input listener to use for handling user input
      * @param windowController the window controller to use for controlling the game window
      */
     @Override
@@ -59,30 +66,38 @@ public class PepseGameManager extends GameManager {
                                UserInputListener inputListener, WindowController windowController) {
 
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
-        skyCreator(windowController.getWindowDimensions());
-        terrainCreator(windowController);
+        this.windowDimensions = windowController.getWindowDimensions();
+        skyCreator();
+        terrainCreator(0, (int) windowDimensions.x());
+        treesCreator(0, (int)windowDimensions.x());
     }
+
     /**
      * Creates a new terrain and adds it to the list of game objects.
-     *
-     * @param windowController the window controller that controls the game window
      */
-    private void terrainCreator(WindowController windowController) {
-        Terrain terrain = new Terrain(this.gameObjects(),Layer.STATIC_OBJECTS,
-                windowController.getWindowDimensions(), TERRAIN_SEED);
-        terrain.createInRange(0, (int) windowController.getWindowDimensions().x());
+    private void terrainCreator(int minX, int maxX) {
+        this.terrain = new Terrain(this.gameObjects(), Layer.STATIC_OBJECTS, windowDimensions, TERRAIN_SEED);
+        terrain.createInRange(minX, maxX);
     }
 
     /**
      * Creates a new sky and adds it to the list of game objects.
-     *
-     * @param windowDimensions the windowDimensions of game
      */
-    private void skyCreator(Vector2 windowDimensions) {
+    private void skyCreator() {
         GameObject sky = Sky.create(gameObjects(), windowDimensions, Layer.BACKGROUND);
         GameObject night = Night.create(gameObjects(), Layer.FOREGROUND, windowDimensions, NIGHT_CYCLE_LEN);
         GameObject sun = Sun.create(gameObjects(), Layer.BACKGROUND, windowDimensions, SUNSET_CYCLE);
         GameObject sunHalo = SunHalo.create(gameObjects(), Layer.BACKGROUND, sun, HALO_COLOR);
+    }
+
+
+    private void treesCreator(int minX, int maxX) {
+        for (int curX = minX; curX <= maxX; curX += 2 * Block.SIZE) {
+            if (Tree.shouldPlantTree()) {
+                float curY = (float) Math.floor(terrain.groundHeightAt(curX) / Block.SIZE) * Block.SIZE;
+                Tree.Create(gameObjects(), new Vector2(curX, curY - Block.SIZE), Layer.STATIC_OBJECTS);
+            }
+        }
     }
 
 
--- src/pepse/world/trees/Tree.java
+++ src/pepse/world/trees/Tree.java
@@ -1,4 +1,51 @@
 package pepse.world.trees;
 
+import danogl.GameObject;
+import danogl.collisions.GameObjectCollection;
+import danogl.gui.rendering.RectangleRenderable;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+import pepse.util.ColorSupplier;
+import pepse.world.Block;
+
+import java.awt.*;
+import java.util.Random;
+
 public class Tree {
+
+    private static final int MINIMUM_TRUNK_HEIGHT = Block.SIZE * 6;
+    private static final int MAXIMUM_TRUNK_HEIGHT = Block.SIZE * 12;
+    private static final int LEAVES_SQUARE_SIZE = Block.SIZE * 8;
+
+    public static final Color BASE_TRUNK_COLOR = new Color(100, 50, 20);
+    public static final Color BASE_LEAF_COLOR = new Color(50, 200, 30);
+
+    private static final float ODDS_TO_PLANT_TREE = 0.2f;
+    private static final int RANDOM_MAX_BOUND = 100;
+    private static final int THRESHOLD = (int) (RANDOM_MAX_BOUND * ODDS_TO_PLANT_TREE);
+
+
+
+    public static void Create(GameObjectCollection gameObjects, Vector2 groundPos, int layer) {
+        // Create the trunk
+        float trunkHeight = getRandomTruckHeight();
+        for (float curY = groundPos.y(); curY >= groundPos.y() - trunkHeight; curY-= Block.SIZE) {
+            Renderable renderable = new RectangleRenderable(ColorSupplier.approximateColor(BASE_TRUNK_COLOR));
+            Block block = new Block(new Vector2(groundPos.x(), curY), renderable);
+            gameObjects.addGameObject(block, layer);
+        }
+
+        // Create the leaves
+        Vector2 center = groundPos.add(new Vector2(0, trunkHeight));
+
+    }
+
+    public static boolean shouldPlantTree() {
+        return new Random().nextInt(RANDOM_MAX_BOUND) < THRESHOLD;
+    }
+
+    public static int getRandomTruckHeight() {
+        return new Random().nextInt(MAXIMUM_TRUNK_HEIGHT - MINIMUM_TRUNK_HEIGHT) + MINIMUM_TRUNK_HEIGHT;
+    }
+
 }

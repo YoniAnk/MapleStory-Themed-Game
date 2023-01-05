7e2c6f80e1b9fe3115a66ca060f4eae7bb39411a
Yoni <yoni.ankri@mail.huji.ac.il>
2023-01-05 17:05:13 +0200

    deleted all magic numbers

--- src/pepse/world/Block.java
+++ src/pepse/world/Block.java
@@ -5,6 +5,10 @@ import danogl.components.GameObjectPhysics;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 
+/**
+ * Represents a block object in the game. This class extends the GameObject class and
+ * has a fixed size and mass. It also has the tag "block".
+ */
 public class Block extends GameObject {
     public static final String BLOCK_TAG = "block";
     public static final int SIZE = 30;
